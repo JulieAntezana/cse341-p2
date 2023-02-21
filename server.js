83d5e688e10b5d88bb8b9cdfab4ac4286b789f53
JulieAntezana <jantezana@byui.edu>
2023-02-21 05:36:52 -0600

    Added auth0

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { auth, requiresAuth } = require('express-openid-connect');
+require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 const app = express();
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
 app
+  .use(auth(config))
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -15,6 +27,15 @@ app
   process.on('uncaughtException', (err, origin) => {
     console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
   });
+
+app.get('/', (req, res) => {
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  });
+
+  app.get('/profile', requiresAuth(), (req, res) => {
+    res.send(JSON.stringify(req.oidc.user));
+  });
+
 
 const db = require('./models');
 db.mongoose
